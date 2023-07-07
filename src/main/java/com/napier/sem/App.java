9d2f569e7a6942109d621d791aa2c007f266c332
Kaung Khant Hein <40614799@live.napier.ac.uk>
2023-07-07 13:04:19 +0630

    random bullshit go 2

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,6 @@ package com.napier.sem;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello there");
+        System.out.println("Hello");
     }
 }
